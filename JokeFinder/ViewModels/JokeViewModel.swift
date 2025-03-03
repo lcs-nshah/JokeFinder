046b19cb5aae53b0bb9aa85f30f32e3d0971bd0c
Neil Shah <nshah@lcs.on.ca>
2025-03-03 14:33:30 -0500

    Added a basic view model to hold the current joke that will be shown by the view.

---
+++ JokeFinder/ViewModels/JokeViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  JokeViewModel.swift
+//  JokeFinder
+//
+//  Created by Neil Shah on 2025-03-03.
+//
+
+import Foundation
+ 
+@Observable
+class JokeViewModel {
+    
+    // MARK: Stored properties
+    
+    // Whatever joke has most recently been downloaded
+    // from the endpoint
+    var currentJoke: Joke?
+    
+    // MARK: Initializer(s)
+    init(currentJoke: Joke? = nil) {
+        
+        // Take whatever joke was provided when an instance of
+        // this view model is created, and make it the current joke.
+        //
+        // Otherwise, the default value for the current joke
+        // will be a nil.
+        self.currentJoke = currentJoke
+    }
+}
